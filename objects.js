cd1d45edba519c6f037614aff03ba5ce078f673b
nmatos46 <nmatos419@hotmail.com>
2018-05-27 01:42:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 
 function updatePlaylist(playlist1, artistName, song){
   playlist1[artistName]=song
-
+  return Object.assign({},playlist1,{[artistName]:[song]})
 }
 
 function removeFromPlaylist(playlist1){
