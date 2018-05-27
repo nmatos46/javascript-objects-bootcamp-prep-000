b0c34488bac2f81cb4414764ca8c2f17c4e1404d
nmatos46 <nmatos419@hotmail.com>
2018-05-27 01:45:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,10 +4,13 @@ var playlist = {
 }
 
 function updatePlaylist(playlist1, artistName, song){
+  //update playlist1 in a destructive manner
   playlist1[artistName]=song
+
+  //return playlist1 object without changing playlist1
   return Object.assign({},playlist1,{[artistName]:[song]})
 }
 
-function removeFromPlaylist(playlist1){
-  return
+function removeFromPlaylist(playlist1, artistName){
+  return delete playlist1[artistName]
 }
