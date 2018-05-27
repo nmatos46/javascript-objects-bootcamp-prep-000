12483d6923f2c16261c1d35a7e10cdce12919daf
nmatos46 <nmatos419@hotmail.com>
2018-05-27 01:41:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,11 @@
 var playlist = {
-  'Slowdive' : 'Alison',
-  'My Bloody Valentine': 'Sometimes'
+  'Queen Bee' : 'Run the World (Girls)',
+  'Spice Girls': 'Wanna Be'
 }
 
 function updatePlaylist(playlist1, artistName, song){
   playlist1[artistName]=song
   return Object.assign({},playlist1,{[artistName]:[song]})
 }
+
+function removeFromPlaylist(playlist1)
