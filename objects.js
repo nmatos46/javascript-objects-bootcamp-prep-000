50856eb59fd3d73d8b2f83f80b97844890965545
nmatos46 <nmatos419@hotmail.com>
2018-05-27 01:27:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
 var playlist = {
   QueenBee: "Run the World (Girls)",
-  SpiceGirls: "Wanna be"
+  Spice_Girls: "Wanna be"
 }
+
+function updatePlaylist(playlist1, artistName, song){
+  return Object.assign({},playlist1,[artistName]:[song])
+} 
